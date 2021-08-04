b6c59c5ca2439cb9026beb6e1c5667edc69a03d8
jcgo <jeancgo@pm.me>
2021-08-04 17:26:31 -0400

    chore: add new dependencies to `requirements.txt` and `setup.py`
    Other changes:
    
    - Update project version from **1.1.0** to **2.5.0**

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@
 from setuptools import setup
 
 setup(
-    name='parse_audits',
-    version='1.1.0',
+    name="parse_audits",
+    version="2.5.0",
     packages=[
-        'parse_audits',
-        'parse_audits.test',
+        "parse_audits",
+        "parse_audits.test",
     ],
-    scripts=['bin/parser_cli.py'],
-    license='LICENSE.txt',
-    description='A tool to parse ClearQuest AuditTrail files to an easier to use format.',
-    long_description=open('README.md').read(),
+    scripts=["bin/cli.py"],
+    license="LICENSE.txt",
+    description="A tool to parse ClearQuest AuditTrail files to an easier-to-use format.",
+    long_description=open("README.md", encoding="utf-8").read(),
     install_requires=[
-        'regex >= 2020.5.14',
-        'typer >= 0.3.2',
+        "regex >= 2020.5.14",
+        "typer >= 0.3.2",
+        "pandas >= 1.3.1",
+        "openpyxl >= 3.0.7",
     ],
 )
