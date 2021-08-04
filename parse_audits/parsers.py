21016191e0f40674182cf8a08a440ef9db2864a9
jcgo <jeancgo@pm.me>
2021-08-04 15:59:58 -0400

    refactor: change implementation of `parse_text_as_csv` completely
    In the resulting csv string of `parse_text_as_csv`, `user_groups` are now separated by a comma, and newline characters are now escaped in 'old' and 'new' values. The function now uses the utility `_convert_dicts_to_csv` which in turn uses the `pandas` library underneath.
    
    Other minor changes to the other functions:
    
    -  `_parse_fields_from_text` return type is `DictIter` making it consistent with the signature of `_parse_entries_from_text`. This change was introduced by the previous commit #90b753d31890be0c889546dd6df31c72de2e2b3f. The return value is now a generator
    
    -  The dictionaries returned by `_parse_entries_from_text` now have an `entry_id` key
    
    BREAKING CHANGE: `_parse_fields_from_text` now returns a generator instead of a list

--- parse_audits/parsers.py
+++ parse_audits/parsers.py
@@ -2,75 +2,66 @@ import json
 from typing import Any, Dict, Generator
 
 from parse_audits.models import ENTRY_PATTERN, FIELD_PATTERN
-from parse_audits.utils import _format_time_string
+from parse_audits.utils import _convert_dicts_to_csv, _format_time_string
 
 
 DictIter = Generator[Dict[Any, Any], None, None]
 
 
 def _parse_fields_from_text(text: str) -> DictIter:
-    """Parse the fields from text to a list of dicts"""
-    fields = [field_match.groupdict("") for field_match in FIELD_PATTERN.finditer(text)]
-
-    return fields
+    """Parse the fields from text to an iter of field dicts"""
+    for field_match in FIELD_PATTERN.finditer(text):
+        yield field_match.groupdict("")
 
 
 def _parse_entries_from_text(text: str) -> DictIter:
-    """
-    Parse the given AuditTrail text and return a list of audit entries.
-    """
-    for entry_match in ENTRY_PATTERN.finditer(text):
-        # Get the entry
+    """Parse the given AuditTrail to an iter of entry dicts."""
+    for entry_id, entry_match in enumerate(ENTRY_PATTERN.finditer(text)):
         entry = entry_match.groupdict("")
 
         entry.update(
             {
+                "entry_id": entry_id,
                 "time": _format_time_string(entry["time"]),
                 "user_groups": entry["user_groups"].split(),
-                "fields": _parse_fields_from_text(entry["fields"]),
+                "fields": list(_parse_fields_from_text(entry["fields"])),
             }
         )
 
         yield entry
 
 
-def parse_text_as_json(audit_text: str) -> str:
-    """Parse the given AuditTrail text and return a JSON string."""
-    return json.dumps(list(_parse_entries_from_text(audit_text)), ensure_ascii=False)
-
-
 def parse_text_as_csv(audit_text: str) -> str:
     """Parse the given AuditTrail text and return a CSV string."""
-    csv_lines = [
-        "'time'|'schema'|'user_name'|'user_login'|'user_groups'|\
-        'action'|'state'|'field_name'|'delta'|'old'|'new'"
+    entries = [
+        {
+            "entry_id": entry["entry_id"],
+            "time": entry["time"],
+            "schema": entry["schema"],
+            "user_name": entry["user_name"],
+            "user_login": entry["user_login"],
+            "user_groups": ",".join(entry["user_groups"]),
+            "action": entry["action"],
+            "state": entry["state"],
+            **field,
+            "old": field["old"].replace("\n", "\\n"),
+            "new": field["new"].replace("\n", "\\n"),
+        }
+        for entry in _parse_entries_from_text(audit_text)
+        for field in entry["fields"]
     ]
 
-    for entry in _parse_entries_from_text(audit_text):
-        fields = entry["fields"]
-
-        for field in fields:
-            csv_lines.append(
-                "|".join(
-                    [
-                        entry["time"],
-                        entry["schema"],
-                        entry["user_name"],
-                        entry["user_login"],
-                        entry["user_groups"],
-                        entry["action"],
-                        entry["state"],
-                        field["field_name"],
-                        field["delta"],
-                        field["old"],
-                        field["new"],
-                    ]
-                )
-            )
-
-    return "\n".join(csv_lines)
+    return _convert_dicts_to_csv(
+        entries,
+        delimiter="|",
+    )
 
 
 def parse_text_as_excel(audit_text: str) -> bytes:
     """Parse the given AuditTrail text and return an Excel spreadsheet."""
     raise NotImplementedError()
+
+
+def parse_text_as_json(audit_text: str) -> str:
+    """Parse the given AuditTrail text and return a JSON string."""
+    return json.dumps(list(_parse_entries_from_text(audit_text)), ensure_ascii=False)
