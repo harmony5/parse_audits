9bf1ea7a8aefb6f5ec24b07c719239fb256da1eb
jcgo <jeancgo@pm.me>
2021-08-04 17:07:04 -0400

    feat(parser): add functionality to parse as Excel and add a new `Parser` class
    Implement the function `parse_text_as_excel` and remove the error raised (`NotImplementedError`). The current implementation uses the `pandas` library underneath.
    
    The resulting excel contains 3 spreadsheets: **Audit Entries**, **Audit Fields** and **User Groups**:
    - **Audit Entries** contains the entries metadata, things like the `time`, `schema`, the user who made the change, the `action` taken and the `state`
    
    - **Audit Fields** contains the fields data and the corresponding `entry_id` and `time`
    
    - **User Groups** contains the `user_name`, `user_login` and the `group_name`s of the users found in the file
    
    Added new `Format` and `Parser` classes. The `Format` class is an enum that represents the currently supported formats to parse the Audit file to, while the `Parser` class acts as a unified interface to all the parser functions, taking the text to parse and the format to parse to.
    
    Other changes:
    
    - Remove old files from `.gitignore`
    
    - Remove `test.py` file as it was used just to test some quick hacks

--- parse_audits/parsers.py
+++ parse_audits/parsers.py
@@ -1,5 +1,10 @@
 import json
-from typing import Any, Dict, Generator
+from dataclasses import dataclass
+from enum import Enum
+from io import BytesIO
+from typing import Any, Dict, Generator, Union
+
+import pandas as pd
 
 from parse_audits.models import ENTRY_PATTERN, FIELD_PATTERN
 from parse_audits.utils import _convert_dicts_to_csv, _format_time_string
@@ -15,7 +20,7 @@ def _parse_fields_from_text(text: str) -> DictIter:
 
 
 def _parse_entries_from_text(text: str) -> DictIter:
-    """Parse the given AuditTrail to an iter of entry dicts."""
+    """Parse the given AuditTrail text to an iter of entry dicts."""
     for entry_id, entry_match in enumerate(ENTRY_PATTERN.finditer(text)):
         entry = entry_match.groupdict("")
 
@@ -58,10 +63,100 @@ def parse_text_as_csv(audit_text: str) -> str:
 
 
 def parse_text_as_excel(audit_text: str) -> bytes:
-    """Parse the given AuditTrail text and return an Excel spreadsheet."""
-    raise NotImplementedError()
+    """Parse the given AuditTrail text and return the result as bytes in Excel format."""
+    entries = []
+    fields = []
+    user_groups = []
+    for entry in _parse_entries_from_text(audit_text):
+        entries.append(
+            {
+                **entry,
+                "user_groups": ",".join(entry["user_groups"]),
+            }
+        )
+        fields.extend(
+            {
+                "entry_id": entry["entry_id"],
+                "time": entry["time"],
+                **field,
+                "old": field["old"].replace("\n", "\\n"),
+                "new": field["new"].replace("\n", "\\n"),
+            }
+            for field in entry["fields"]
+        )
+        for group in entry["user_groups"]:
+            row = {
+                "user_name": entry["user_name"],
+                "user_login": entry["user_login"],
+                "group_name": group,
+            }
+            if row not in user_groups:
+                user_groups.append(row)
+
+    entries_df = pd.DataFrame(
+        entries,
+        columns=[
+            "entry_id",
+            "time",
+            "schema",
+            "user_name",
+            "user_login",
+            "user_groups",
+            "action",
+            "state",
+        ],
+    )
+    fields_df = pd.DataFrame(fields)
+    groups_df = pd.DataFrame(user_groups)
+
+    with BytesIO() as buffer:
+        with pd.ExcelWriter(buffer) as writer:
+            entries_df.to_excel(writer, sheet_name="Audit Entries", index=False)
+            fields_df.to_excel(writer, sheet_name="Audit Fields", index=False)
+            groups_df.to_excel(writer, sheet_name="User Groups", index=False)
+
+        return buffer.getvalue()
 
 
 def parse_text_as_json(audit_text: str) -> str:
     """Parse the given AuditTrail text and return a JSON string."""
     return json.dumps(list(_parse_entries_from_text(audit_text)), ensure_ascii=False)
+
+
+class Format(str, Enum):
+    """Represents the format of the parsed data."""
+
+    CSV = "csv"
+    EXCEL = "xlsx"
+    JSON = "json"
+
+
+@dataclass
+class Parser:
+    """Parser for AuditTrail text."""
+
+    audit_text: str
+    format: Format
+
+    def as_csv(self) -> str:
+        """Return the parsed AuditTrail text as CSV."""
+        return parse_text_as_csv(self.audit_text)
+
+    def as_excel(self) -> bytes:
+        """Return the parsed AuditTrail text as Excel."""
+        return parse_text_as_excel(self.audit_text)
+
+    def as_json(self) -> str:
+        """Return the parsed AuditTrail text as JSON."""
+        return parse_text_as_json(self.audit_text)
+
+    def parse(self) -> Union[bytes, str]:
+        """Parse the given AuditTrail text according to the given format and return the result."""
+        if self.format == Format.CSV:
+            return parse_text_as_csv(self.audit_text)
+        elif self.format == Format.EXCEL:
+            return parse_text_as_excel(self.audit_text)
+        elif self.format == Format.JSON:
+            return parse_text_as_json(self.audit_text)
+        else:
+            raise ValueError(f"Unknown format: {self.format}")
